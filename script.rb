ea7172aae80e43d15f50ef571cd2422c89c4a983
theprincesbride <140436808+theprincesbride@users.noreply.github.com>
2023-11-03 00:44:48 -0400

    Made a functioning method for checking if a letter is included in the secret word or not.  Added a method that adds the letter to correctly chosen letter array and hidden word if true.

--- script.rb
+++ script.rb
@@ -10,7 +10,15 @@ def game()
     puts "Welcome to Hangman!  You will be attempting to guess the secret word within 11 turns by guessing 1 letter at a time."
     puts "When the floor appears below the hangman, you lose!"
     puts secret_word
+
+    until player.guesses == 0
     player.display_values(secret_word_hidden, player.guesses, player.correct_letters, player.wrong_letters)
+    letter = player.get_letter()
+    result = player.right_letter?(letter, secret_word)
+    if result == true
+        secret_word_hidden = player.true_action(letter, secret_word, secret_word_hidden)
+    end
+    end
 end
 
 
@@ -27,7 +35,51 @@ class Player
         puts "Number of guesses left: #{value2}"
         puts "Correct letters guessed: #{value3}"
         puts "Wrong letters guessed #{value4}"
+        sleep 2
     end
+    def get_letter()
+        puts "Please choose a letter.  The secret word does not include numbers or symbols."
+        letter = gets
+        letter = letter.chomp.downcase
+        if !@letters.include?(letter)
+            get_letter()
+        elsif @letters.include?(letter)
+            letter
+        end
+    end
+
+    def right_letter?(letter, secret_word) #need to include action for a repeated selection
+        if secret_word.include?(letter)
+            puts "Congratulations!  You chose one of the correct letters!"
+            sleep 2
+            true
+        elsif !secret_word.include?(letter)
+            false
+        end
+    end
+
+    def true_action(letter, secret_word, secret_word_hidden)
+        secret_array = secret_word_hidden.split(' ')
+        i = 0
+        while i < secret_word.length
+                if secret_word[i] == letter && !@correct_letters.include?(letter)
+                    @correct_letters.push(letter)
+                    secret_array.delete_at(i)
+                    secret_array.insert(i, letter)
+                    i += 1
+                elsif secret_word[i] == letter
+                    secret_array.delete_at(i)
+                    secret_array.insert(i, letter)
+                    i += 1
+                else
+                    i += 1
+                end
+
+            end
+            @guesses -= 1
+            secret_array.join(' ')
+        end
+
 
 end
 
