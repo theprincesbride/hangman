46ae035fbe389210a28a0fa4d6e7c3064320477f
theprincesbride <140436808+theprincesbride@users.noreply.github.com>
2023-11-02 23:18:21 -0400

    Made stick figure display of the hangman.  Made Player class to track correct & wrong guesses and Hangman class to track hangman display progress

--- script.rb
+++ script.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+def game()
 dictionary = []
 
 File.open('dictionary.txt', 'r') do |f|
@@ -8,5 +10,58 @@ File.open('dictionary.txt', 'r') do |f|
         end
     end
 end
+    secret_word = dictionary.sample
+    player = Player.new
+    hangman = Hangman.new
+    puts hangman.hangman_display
+    puts "Welcome to Hangman!  You will be attempting to guess the secret word within 11 turns by guessing 1 letter at a time."
+    puts "When the floor appears below the hangman, you lose!"
+    secret_word_hidden = hide_secret_word(secret_word)
+    puts secret_word
+    puts "Word to guess: #{secret_word_hidden}"
+end
 
-secret_word = dictionary.sample
+
+def hide_secret_word(word)
+    new_string = ''
+    i = 0
+    while i < word.length do
+        new_string += '_ '
+        i += 1
+    end
+    new_string
+end
+
+
+class Player
+    attr_accessor :letters, :guesses, :correct_letters, :wrong_letters
+    def initialize()
+        @letters = 'abcdefghijklmnopqrstuvwxyz'
+        @guesses = 11
+        @correct_letters = []
+        @wrong_letters = []
+    end
+end
+
+class Hangman
+    attr_accessor :hangman_display, :hangman_tracker
+    def initialize()
+    @hangman_display = [
+    [" ______________________________"],
+    ["|                             |"],
+    ["|            [   ]            |"],
+    ["|              |              |"],
+    ["|          --- | ---          |"],
+    ["|              |              |"],
+    ["|              |              |"],
+    ["|             / \\             |"],
+    ["|            /   \\            |"],
+    ["|          _/     \\_          |"],
+    ["|_____________________________|"]
+    ]
+    end
+    @hangman_tracker = []
+
+end
+
+game()
